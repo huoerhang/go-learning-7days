8d61b1627b8542732136de60205b108594b7e007
CV Developer <huohaihang@126.com>
2021-07-04 13:13:56 +0800

    gee.go

--- Gee/day1-http-base/base3/Gee/gee.go
+++ Gee/day1-http-base/base3/Gee/gee.go
@@ -5,37 +5,37 @@ import (
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter,*http.Request)
+type HandlerFunc func(http.ResponseWriter, *http.Request)
 
-type  Engine struct {
+type Engine struct {
 	router map[string]HandlerFunc
 }
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter,req *http.Request)  {
-	key:=req.Method+"-"+req.URL.Path
-	if handler,ok:=engine.router[key];ok{
-		handler(w,req)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
 		return
 	}
-	fmt.Fprintf(w,"404 NOT FOUND:%s\n",req.URL)
+	fmt.Fprintf(w, "404 NOT FOUND:%s\n", req.URL)
 }
 
 func New() *Engine {
 	return &Engine{router: make(map[string]HandlerFunc)}
 }
 
-func (engine *Engine) addRoute(method string,pattern string,handler HandlerFunc)  {
-	key:=method+"-"+pattern
-	engine.router[key]=handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
 }
 
-func  (engine Engine) GET(pattern string,handler HandlerFunc)  {
-	engine.addRoute("GET",pattern,handler)
+func (engine Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
-func (engine Engine) POST(pattern string,handler HandlerFunc)  {
-	engine.addRoute("POST",pattern,handler)
+func (engine Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
-	return  http.ListenAndServe(addr,engine)
+	return http.ListenAndServe(addr, engine)
 }
